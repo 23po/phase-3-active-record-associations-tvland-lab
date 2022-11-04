4f386e0f6e04423378cb29e7bdb5f987d3597f4a
calvin <ckodalo@gmail.com>
2022-11-04 17:42:08 +0300

    commit tree e3a092be52d67334e811596c7edcb7294584ad85

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+   has_many :characters
+   has_many :actors, through: characters
+   belongs_to :network
+
+   def actors_list
+    self.actors
+    # self.actors.map do |character|
+    #  "#{character.first_name} #{character.last_name}"
+    # end
+   end
+
 end
